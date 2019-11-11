a26f6bf68da6de1bc8bc488afc03f7c0f2379436
Charles-Antoine LEGER <toutane.leger@gmail.com>
2019-11-11 11:32:38 +0100

    starting header animation

--- src/views/Explore.js
+++ src/views/Explore.js
@@ -1,31 +1,205 @@
-import React from "react";
-import { Button, View, Text } from "react-native";
-class Explore extends React.Component {
-  static navigationOptions = {
-    title: "Explore"
+import React, { useState, useContext, useEffect } from "react";
+import { Button, View, Text, ScrollView, Animated } from "react-native";
+import { BlurView } from "expo-blur";
+import { ThemeContext } from "../contexts/ThemeContext";
+import { screenWidth } from "../utils/dimensions";
+
+export default Explore = props => {
+  const { theme } = useContext(ThemeContext);
+  const [scrollY, setScrollY] = useState(new Animated.Value(0));
+  _getHeaderOpacity = () => {
+    return scrollY.interpolate({
+      inputRange: [0, 30, 60, 100],
+      outputRange: [0, 0, 1, 1],
+      extrapolate: "clamp",
+      useNativeDriver: true
+    });
   };
-  render() {
-    return (
-      <View
+  _getHeaderBlackOpacity = () => {
+    return scrollY.interpolate({
+      inputRange: [0, 35, 50, 100],
+      outputRange: [1, 1, 0, 0],
+      extrapolate: "clamp",
+      useNativeDriver: true
+    });
+  };
+  _getTitleOpacity = () => {
+    return scrollY.interpolate({
+      inputRange: [0, 32, 33, 100],
+      outputRange: [1, 1, 0, 0],
+      extrapolate: "clamp",
+      useNativeDriver: true
+    });
+  };
+  _getHeaderTitleOpacity = () => {
+    return scrollY.interpolate({
+      inputRange: [0, 12, 70, 100],
+      outputRange: [0, 0, 1, 1],
+      extrapolate: "clamp",
+      useNativeDriver: true
+    });
+  };
+  const headerOpacity = this._getHeaderOpacity();
+  const headerBlackOpacity = this._getHeaderBlackOpacity();
+  const titleOpacity = this._getTitleOpacity();
+  const headerTitleOpacity = this._getHeaderTitleOpacity();
+
+  return (
+    <View style={{ backgroundColor: theme.backgroundColor }}>
+      <ScrollView
+        style={{ height: 1000, zIndex: 1 }}
+        onScroll={Animated.event([
+          { nativeEvent: { contentOffset: { y: scrollY } } }
+        ])}
+        scrollEventThrottle={16}
+        snapToAlignment={"start"}
+        snapToInterval={70}
+      >
+        <View
+          style={{
+            marginHorizontal: 32,
+            marginTop: 100,
+            justifyContent: "center"
+          }}
+        >
+          <Animated.Text
+            style={{
+              opacity: titleOpacity,
+              fontSize: 34,
+              fontFamily: "sf-display-bold",
+              color: theme.fontColor
+            }}
+          >
+            Explore
+          </Animated.Text>
+          <View
+            style={{
+              marginTop: 60,
+              height: 200,
+              width: 330,
+              backgroundColor: theme.gray5,
+              borderRadius: 10,
+              justifyContent: "center"
+            }}
+          >
+            <Button
+              title="Go to Search screen"
+              color={theme.buttonColor}
+              onPress={() => props.navigation.navigate("Search")}
+            />
+            <Button
+              title="Go to Account screen"
+              color={theme.buttonColor}
+              onPress={() => props.navigation.navigate("Account")}
+            />
+          </View>
+          <View
+            style={{
+              marginTop: 30,
+              height: 200,
+              width: 330,
+              backgroundColor: theme.gray5,
+              borderRadius: 10
+            }}
+          />
+          <View
+            style={{
+              marginTop: 30,
+              height: 200,
+              width: 330,
+              backgroundColor: "white",
+              borderRadius: 10
+            }}
+          />
+          <View
+            style={{
+              marginTop: 30,
+              height: 200,
+              width: 330,
+              backgroundColor: theme.gray5,
+              borderRadius: 10
+            }}
+          />
+          <View
+            style={{
+              marginTop: 30,
+              height: 200,
+              width: 330,
+              backgroundColor: theme.gray5,
+              borderRadius: 10
+            }}
+          />
+          <View
+            style={{
+              marginTop: 30,
+              height: 200,
+              width: 330,
+              backgroundColor: theme.gray5,
+              borderRadius: 10
+            }}
+          />
+          <View
+            style={{
+              marginTop: 30,
+              height: 200,
+              width: 330,
+              backgroundColor: theme.gray5,
+              borderRadius: 10
+            }}
+          />
+        </View>
+      </ScrollView>
+      <Animated.View
         style={{
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center"
+          zIndex: 100,
+          position: "absolute",
+          top: 0,
+          opacity: headerOpacity
         }}
       >
-        <Text style={{ fontSize: 60, fontFamily: "sf-display-bold" }}>
+        <BlurView
+          tint={theme.theme}
+          intensity={200}
+          style={{
+            zIndex: 5,
+            height: 100,
+            width: screenWidth
+          }}
+        />
+      </Animated.View>
+      <Animated.View
+        style={{
+          zIndex: 50,
+          height: 100,
+          width: screenWidth,
+          backgroundColor: theme.backgroundColor,
+          position: "absolute",
+          top: 0,
+          opacity: headerBlackOpacity
+        }}
+      ></Animated.View>
+      <View
+        style={{
+          zIndex: 200,
+          height: 100,
+          width: screenWidth,
+          alignItems: "center",
+          justifyContent: "center",
+          position: "absolute",
+          top: 15
+        }}
+      >
+        <Animated.Text
+          style={{
+            fontSize: 17,
+            fontFamily: "sf-text-semibold",
+            color: theme.fontColor,
+            opacity: headerTitleOpacity
+          }}
+        >
           Explore
-        </Text>
-        <Button
-          title="Go to Search screen"
-          onPress={() => this.props.navigation.navigate("Search")}
-        />
-        <Button
-          title="Go to Account screen"
-          onPress={() => this.props.navigation.navigate("Account")}
-        />
+        </Animated.Text>
       </View>
-    );
-  }
-}
-export default Explore;
+    </View>
+  );
+};
