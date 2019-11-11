ec1ab4266f6be27c514fe8de3d58a8053086fe48
Charles-Antoine LEGER <toutane.leger@gmail.com>
2019-11-11 11:36:56 +0100

    add firebase config into gitignore

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,16 +2,7 @@ import app from "firebase/app";
 import "firebase/auth";
 import "firebase/firebase-firestore";
 
-const config = {
-  apiKey: "AIzaSyB-2qdqMzOLUR8igM2O96yhGJLqZdHS5-8",
-  authDomain: "awesome-app-cd909.firebaseapp.com",
-  databaseURL: "https://awesome-app-cd909.firebaseio.com",
-  projectId: "awesome-app-cd909",
-  storageBucket: "awesome-app-cd909.appspot.com",
-  messagingSenderId: "196712668296",
-  appId: "1:196712668296:web:bbd5a155d227dfa052133d",
-  measurementId: "G-41XD5E4N8C"
-};
+import { config } from "../config/firebaseConfig";
 
 class Firebase {
   constructor() {
