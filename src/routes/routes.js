67d0a622c1b3821ac973dcc583789f52a5df1f99
Charles-Antoine LEGER <toutane.leger@gmail.com>
2019-11-11 13:49:38 +0100

    theme works now with tabbar

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
-import { View, Text } from "react-native";
+import React, { Component, useContext } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { createStackNavigator } from "react-navigation-stack";
 import { Ionicons } from "@expo/vector-icons";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 import ExploreStack from "./ExploreNavigator";
 
@@ -11,6 +12,66 @@ import Library from "../views/Library";
 import Explore from "../views/Explore";
 import Search from "../views/Search";
 
+const TabBar = props => {
+  const { theme } = useContext(ThemeContext);
+  const {
+    renderIcon,
+    getLabelText,
+    activeTintColor,
+    inactiveTintColor,
+    onTabPress,
+    onTabLongPress,
+    getAccessibilityLabel,
+    navigation
+  } = props;
+
+  const { routes, index: activeRouteIndex } = navigation.state;
+
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        height: 85,
+        backgroundColor:
+          theme.theme === "dark" ? "rgb(17, 17, 17)" : "rgb(242, 242, 247)"
+      }}
+    >
+      {routes.map((route, routeIndex) => {
+        const isRouteActive = routeIndex === activeRouteIndex;
+        const tintColor = isRouteActive ? activeTintColor : inactiveTintColor;
+
+        return (
+          <TouchableOpacity
+            activeOpacity={1}
+            key={routeIndex}
+            style={{ flex: 1, justifyContent: "start", alignItems: "center" }}
+            onPress={() => {
+              onTabPress({ route });
+            }}
+            onLongPress={() => {
+              onTabLongPress({ route });
+            }}
+            accessibilityLabel={getAccessibilityLabel({ route })}
+          >
+            {renderIcon({ route, focused: isRouteActive, tintColor })}
+            <Text
+              style={{
+                fontWeight: "500",
+                fontSize: 10,
+                marginTop: 2,
+                color: isRouteActive
+                  ? "rgb(100, 210, 255)"
+                  : "rgb(142, 142, 147)"
+              }}
+            >
+              {getLabelText({ route })}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+};
 const TabBarNavigator = createBottomTabNavigator(
   {
     Library: {
@@ -23,8 +84,9 @@ const TabBarNavigator = createBottomTabNavigator(
       screen: Search
     }
   },
+
   {
-    defaultNavigationOptions: ({ navigation, props }) => ({
+    defaultNavigationOptions: ({ navigation, screenProps }) => ({
       tabBarIcon: ({ focused, horizontal, tintColor }) => {
         const { routeName } = navigation.state;
         routeName === "Library"
@@ -38,27 +100,19 @@ const TabBarNavigator = createBottomTabNavigator(
             name={iconName}
             size={28}
             color={tintColor}
-            style={{ marginTop: 6 }}
+            style={{ marginTop: 10 }}
           />
         );
       }
     }),
     initialRouteName: "Explore",
-    tabBarOptions: {
-      style: {
-        //light
-        // backgroundColor: "rgb(242, 242, 247)",
-        // dark
-        backgroundColor: "rgb(17, 17, 17)",
-        borderTopColor: "transparent"
-      },
-      activeTintColor: {
-        light: "rgb(100, 210, 255)",
-        dark: "rgb(90, 200, 255)"
-      },
-      inactiveTintColor: "rgb(142, 142, 147)",
-      labelStyle: { fontWeight: "500", fontSize: 10, marginTop: 2 }
-    }
+    tabBarComponent: props => (
+      <TabBar
+        {...props}
+        activeTintColor="rgb(100, 210, 255)"
+        inactiveTintColor="rgb(142, 142, 147)"
+      />
+    )
   }
 );
 
