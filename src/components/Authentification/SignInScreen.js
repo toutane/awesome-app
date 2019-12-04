beedd7b31f97a49de4ba98706598f648f261f71a
Charles-Antoine LEGER <toutane.leger@gmail.com>
2019-12-04 21:32:35 +0100

    update auth text input component

--- src/components/Authentification/SignInScreen.js
+++ src/components/Authentification/SignInScreen.js
@@ -4,5 +4,31 @@ import { View } from "react-native";
 import TextInput from "./TextInput";
 
 export default SignInScreen = props => {
-  return <View></View>;
+  return (
+    <View>
+      <TextInput
+        borderColor={props.emailInputColor}
+        placeholder={"email address"}
+        autofocus={false}
+        returnKeyType="next"
+        keyboardType="email-address"
+        onSubmitEditing={() => props.onSubmitEditing}
+        onChangeText={e => props.onChangeText}
+      />
+      <TextInput
+        borderColor={props.borderColor}
+        placeholder={"password"}
+        returnKeyType="go"
+      />
+    </View>
+  );
 };
+
+// style={{ borderColor: props.borderColor }}
+// autoCapitalize="none"
+// placeholder={props.placeholder}
+// autoFocus={props.autoFocus}
+// returnKeyType={props.returnKeyType}
+// keyboardType={props.keyboardType}
+// onSubmitEditing={props.onSubmitEditing}
+// onChangeText={e => props.onChangeText(e)}
