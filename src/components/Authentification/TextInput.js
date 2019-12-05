0e97e08b7088d305403514fb51ccb5a242b9c75f
Charles-Antoine LEGER <toutane.leger@gmail.com>
2019-12-05 22:47:02 +0100

    update text input styled component

--- src/components/Authentification/TextInput.js
+++ src/components/Authentification/TextInput.js
@@ -3,7 +3,7 @@ import { View } from "react-native";
 
 import styled from "styled-components";
 
-const TextInputStyled = styled.TextInput`
+export default TextInput = styled.TextInput`
   height: 50;
   border-color: rgba(0, 0, 0, 0.1);
   border-width: 1;
@@ -11,20 +11,3 @@ const TextInputStyled = styled.TextInput`
   padding-horizontal: 10;
   font-size: 18;
 `;
-
-export default TextInput = props => {
-  return (
-    <View>
-      <TextInputStyled
-        style={{ borderColor: props.borderColor }}
-        autoCapitalize="none"
-        placeholder={props.placeholder}
-        autoFocus={props.autoFocus}
-        returnKeyType={props.returnKeyType}
-        keyboardType={props.keyboardType}
-        onSubmitEditing={props.onSubmitEditing}
-        onChangeText={e => props.onChangeText(e)}
-      />
-    </View>
-  );
-};
