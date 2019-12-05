a270e51efa799b8db7a5e260e9817a9816ee4a27
Charles-Antoine LEGER <toutane.leger@gmail.com>
2019-12-05 22:47:21 +0100

    update signin component

--- src/components/Authentification/SignIn.js
+++ src/components/Authentification/SignIn.js
@@ -16,23 +16,26 @@ export default SignIn = props => {
   const [scrollY, setScrollY] = useState(new Animated.Value(0));
 
   const [email, setEmail] = useState("email@email.com");
-  const [emailInputColor, setEmailInputColor] = useState("");
+  const [emailInputColor, setEmailInputColor] = useState(theme.gray3);
   const [password, setPassword] = useState("123456");
-  const [passwordInputColor, setPasswordInputColor] = useState("");
+  const [passwordInputColor, setPasswordInputColor] = useState(theme.gray3);
   const [error, setError] = useState("");
 
   async function SignIn(email, password) {
     if (email !== "") {
       if (password !== "") {
-        login(email, password).catch(error =>
-          error.code === "auth/invalid-email"
-            ? (setEmailInputColor("red"), setError("email is badly formatted"))
-            : error.code === "auth/wrong-password"
-            ? (setPasswordInputColor("red"), setError("password is incorrect"))
-            : error.code === "auth/user-not-found"
-            ? (setEmailInputColor("red"), setError("user not found"))
-            : alert(error.code)
-        );
+        login(email, password).catch(error => {
+          console.log(error),
+            error.code === "auth/invalid-email"
+              ? (setEmailInputColor("red"),
+                setError("email is badly formatted"))
+              : error.code === "auth/wrong-password"
+              ? (setPasswordInputColor("red"),
+                setError("password is incorrect"))
+              : error.code === "auth/user-not-found"
+              ? (setEmailInputColor("red"), setError("user not found"))
+              : alert(error.code);
+        });
         props.navigation.navigate("Home");
       } else {
         setPasswordInputColor("red"), setError("password is required");
@@ -82,8 +85,62 @@ export default SignIn = props => {
         header="Explore"
         scrollY={scrollY}
       />
-      <SignInScreen />
-      <View style={{ marginTop: 100 }}>
+      <SignInScreen
+        {...props}
+        theme={theme}
+        error={error}
+        emailInputColor={emailInputColor}
+        email={email}
+        passwordInputColor={passwordInputColor}
+        password={password}
+        onChangeEmail={e => {
+          setEmail(e), setEmailInputColor(theme.gray4);
+        }}
+        onChangePassword={e => {
+          setPassword(e), setPasswordInputColor(theme.gray4);
+        }}
+        onSubmitEditing={() => SignIn(email, password)}
+      />
+      <View style={{ alignItems: "center", marginTop: 30 }}>
+        <Text
+          style={{
+            color: theme.fontColor,
+            fontFamily: "sf-text-medium"
+          }}
+        >
+          Don't have an account ?
+          <Text
+            style={{
+              color: theme.buttonColor,
+              fontFamily: "sf-display-bold",
+              fontSize: 20
+            }}
+            onPress={
+              () => props.navigation.navigate("SignUp")
+              // () =>
+              // this.setState(
+              //   {
+              //     error: "null",
+              //     emailInputColor: "rgba(0, 0, 0, 0.1)",
+              //     passwordInputColor: "rgba(0, 0, 0, 0.1)"
+              //   },
+              // )
+            }
+          >
+            {" "}
+            Sign up
+          </Text>
+        </Text>
+        <Text
+          style={{
+            color: error !== "null" ? "red" : "rgba(0, 0, 0, 0)",
+            marginTop: 10
+          }}
+        >
+          {error}
+        </Text>
+      </View>
+      {/* <View style={{ marginTop: 100 }}>
         <Text
           style={{
             textAlign: "center",
@@ -114,7 +171,7 @@ export default SignIn = props => {
           color={theme.buttonColor}
           onPress={() => logout()}
         />
-      </View>
+      </View> */}
     </View>
   );
 };
