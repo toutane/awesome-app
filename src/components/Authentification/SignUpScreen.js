f7d510a221f454b4fffe9f51cde51906cb4f4c1c
Charles-Antoine LEGER <toutane.leger@gmail.com>
2019-12-10 22:48:01 +0100

    create signupscreen component

---
+++ src/components/Authentification/SignUpScreen.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { View } from "react-native";
+import { Text, Button } from "native-base";
+import TextInput from "./TextInput";
+
+export default SignUpScreen = props => {
+  return (
+    <View style={{ paddingHorizontal: 45, marginTop: 220 }}>
+      <TextInput
+        style={{
+          borderColor: props.emailInputColor,
+          color: props.theme.fontColor
+        }}
+        placeholder={"email address"}
+        placeholderTextColor={props.theme.fontColor}
+        autoCapitalize="none"
+        autoFocus={false}
+        returnKeyType="next"
+        keyboardType="email-address"
+        onSubmitEditing={() => passwordInput.focus()}
+        onChangeText={e => props.onChangeEmail(e)}
+      />
+      <View style={{ marginTop: 20 }}>
+        <TextInput
+          style={{
+            borderColor: props.passwordInputColor,
+            color: props.theme.fontColor
+          }}
+          autoCapitalize="none"
+          placeholder={"password"}
+          secureTextEntry
+          placeholderTextColor={props.theme.fontColor}
+          returnKeyType="go"
+          ref={input => (passwordInput = input)}
+          onSubmitEditing={() => props.onSubmitEditing()}
+          onChangeText={e => props.onChangePassword(e)}
+        />
+      </View>
+      <View style={{ marginTop: 30 }}>
+        <Button
+          block
+          onPress={() => props.onSubmitEditing()}
+          style={{
+            height: 50,
+            backgroundColor: props.theme.buttonColor,
+            borderRadius: 10
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 20,
+              fontWeight: "bold",
+              color: "white"
+            }}
+          >
+            Sign in
+          </Text>
+        </Button>
+      </View>
+    </View>
+  );
+};
